d8d7b82bda2989784251b04c6e8526def6324816
CiaranOtter <62837202+CiaranOtter@users.noreply.github.com>
2022-07-14 11:33:31 +0200

    added graphics to the map page

--- src/config.js
+++ src/config.js
@@ -85,8 +85,7 @@ var config = {
       alignment: 'left',
       hidden: false,
       title: '',
-      image: './images/Oil_production_by_aAfrican_Opec_Members.png',
-      description:'<p>The bulk of the rest of the Chinese financing is shared between five hydro-electric power projects. </p><p>There are more than 300 projects listed in Angola in the dataset released by AidData in September 2021, including multiple ports, roads and railways, airports, an oil field and refinery projects.</p><p>Not only is the country oil-rich but its ports are gateways for raw materials mined in landlocked countries such as the Democratic Republic of Congo (DRC) and Zambia.</p><p>For example, $715-million went towards rehabilitating the 1,350km-long Benguela Railway line that connects Angola’s port of Lobito with Luau on the other side of the country on the border with DRC.</p><p>The railway line facilitates the transportation of cobalt from mines in the DRC and copper from Zambia to the Atlantic port for export around the world.</p>',
+      description:'<p><div class="flourish-embed flourish-chart" data-src="visualisation/10545142"><script src="https://public.flourish.studio/resources/embed.js"></script></div></p><p>The bulk of the rest of the Chinese financing is shared between five hydro-electric power projects. </p><p>There are more than 300 projects listed in Angola in the dataset released by AidData in September 2021, including multiple ports, roads and railways, airports, an oil field and refinery projects.</p><p>Not only is the country oil-rich but its ports are gateways for raw materials mined in landlocked countries such as the Democratic Republic of Congo (DRC) and Zambia.</p><p>For example, $715-million went towards rehabilitating the 1,350km-long Benguela Railway line that connects Angola’s port of Lobito with Luau on the other side of the country on the border with DRC.</p><p>The railway line facilitates the transportation of cobalt from mines in the DRC and copper from Zambia to the Atlantic port for export around the world.</p>',
       location: {
         center: [13.2833199, -11.1799557],
         zoom: 5.5,
@@ -142,9 +141,8 @@ var config = {
       alignment: 'right',
       hidden: false,
       title: 'Bata Port',
-      image: './images/Highest_GDP_african_Countries.png',
       description:
-        '<p>An oil-backed loan agreement between China’s Eximbank and the government of Equatorial Guinea helped to finance the rehabilitation and modernisation of the port of Bata into a large, deep-water port. It was officially inaugurated in 2019.</p><p>At the end of 2021 the Wall Street Journal reported that China was negotiating with authorities in Equatorial Guinea to build a <a href="https://www.wsj.com/articles/china-seeks-first-military-base-on-africas-atlantic-coast-u-s-intelligence-finds-11638726327" target="_blank">naval base near Bata</a>, which the United States government is not pleased about given Equatorial Guinea’s position on the Atlantic Ocean.</p><p>If the plan were to go ahead, it would be China’s second military base in Africa.</p><p>China also has a naval base in tiny Djibouti, on the East African coast, next to the <a href="https://china.aiddata.org/projects/59375/" target="_blank">Port of Doraleh</a>. Work was also completed at that port in 2017 by Chinese companies with Chinese financing.</p>',
+        '<p><div class="flourish-embed flourish-chart" data-src="visualisation/10545218"><script src="https://public.flourish.studio/resources/embed.js"></script></div></p><p>An oil-backed loan agreement between China’s Eximbank and the government of Equatorial Guinea helped to finance the rehabilitation and modernisation of the port of Bata into a large, deep-water port. It was officially inaugurated in 2019.</p><p>At the end of 2021 the Wall Street Journal reported that China was negotiating with authorities in Equatorial Guinea to build a <a href="https://www.wsj.com/articles/china-seeks-first-military-base-on-africas-atlantic-coast-u-s-intelligence-finds-11638726327" target="_blank">naval base near Bata</a>, which the United States government is not pleased about given Equatorial Guinea’s position on the Atlantic Ocean.</p><p>If the plan were to go ahead, it would be China’s second military base in Africa.</p><p>China also has a naval base in tiny Djibouti, on the East African coast, next to the <a href="https://china.aiddata.org/projects/59375/" target="_blank">Port of Doraleh</a>. Work was also completed at that port in 2017 by Chinese companies with Chinese financing.</p>',
       location: {
         center: [9.7368403, 1.8236817],
         zoom: 12,
@@ -218,9 +216,8 @@ var config = {
       alignment: 'right',
       hidden: false,
       title: 'Zungeru Hydroelectric Power Plant',
-      image: './images/Nigeria_Electricty_access.png',
       description:
-        '<p>The <a href="https://www.thisdaylive.com/index.php/2022/03/04/fg-nigerias-installed-electricity-capacity-stands-at-18000mw-generates-8000mw-daily/" target="_blank">energy minister recently stated</a> that the country has 18GW of installed capacity, but typically only 8GW is delivered.</p><p>Nigeria ranks sixth in Africa for the amount of electricity generated in terrawatt hours, a long way behind South Africa and Egypt, according to energy think tank Ember, even though its population is significantly bigger.</p><p>Ironically, oil and natural gas are Nigeria’s <a href="https://nigerianstat.gov.ng/elibrary/read/1241099" target="_blank">top-two export products</a>.  Nigeria is Africa’s largest producer of crude oil and 75% of the country’s export income is from crude oil. Most of the country’s electricity is generated from natural gas. Yet, not enough is being produced to meet the country’s needs</p><p>The country still has to import petrol because of  lack of <a href="https://www.dataphyte.com/latest-reports/development/how-nigerias-high-petrol-importation-and-dead-refineries-affect-its-struggling-economy/#:~:text=Already%2C%20refined%20petroleum%20takes%20the,of%20the%20total%20import%20value." target="_blank">refining capacity</a>. “Motor spirit” or petrol is the country’s biggest import and millions of litres of it is needed to power the generators.</p>',
+        '<p><div class="flourish-embed flourish-scatter" data-src="visualisation/10545424"><script src="https://public.flourish.studio/resources/embed.js"></script></div></p><p>The <a href="https://www.thisdaylive.com/index.php/2022/03/04/fg-nigerias-installed-electricity-capacity-stands-at-18000mw-generates-8000mw-daily/" target="_blank">energy minister recently stated</a> that the country has 18GW of installed capacity, but typically only 8GW is delivered.</p><p>Nigeria ranks sixth in Africa for the amount of electricity generated in terrawatt hours, a long way behind South Africa and Egypt, according to energy think tank Ember, even though its population is significantly bigger.</p><p>Ironically, oil and natural gas are Nigeria’s <a href="https://nigerianstat.gov.ng/elibrary/read/1241099" target="_blank">top-two export products</a>.  Nigeria is Africa’s largest producer of crude oil and 75% of the country’s export income is from crude oil. Most of the country’s electricity is generated from natural gas. Yet, not enough is being produced to meet the country’s needs</p><p>The country still has to import petrol because of  lack of <a href="https://www.dataphyte.com/latest-reports/development/how-nigerias-high-petrol-importation-and-dead-refineries-affect-its-struggling-economy/#:~:text=Already%2C%20refined%20petroleum%20takes%20the,of%20the%20total%20import%20value." target="_blank">refining capacity</a>. “Motor spirit” or petrol is the country’s biggest import and millions of litres of it is needed to power the generators.</p>',
       location: {
         center: [6.2932451, 9.9005702],
         zoom: 12,
@@ -245,9 +242,8 @@ var config = {
       alignment: 'right',
       hidden: false,
       title: 'Zungeru Hydroelectric Power Plant',
-      image: './images/african_countries_that generate_the_most_electricity.png',
       description:
-        '<p>Enter China. The Chinese have already provided finance for two gas power plants in Nigeria.</p><p>But one of its biggest investments in the energy sector is the new 700MW Zungera Hydroelectric dam located in Niger State, which is due to come on stream in 2022. It will meet close to 10% of Nigeria’s total domestic energy needs, according to Nigeria’s <a href="https://www.bpe.gov.ng/zungeru-hydropower-project/" target="_blank">Bureau of Public Enterprise</a></p><p>Zungera is one of 47 hydroelectric power projects China has been involved with in Africa since 2000, according to AidData.</p><p>These projects range in size from the massive 2,172MW Caculo Cabaca Hydroelectric power station in Angola to the 0.64MW Makali plant in Sierra Leone. Together they have a potential generating capacity of close to 12,000MW.</p><p>Zungera is the sixth largest of Chinese-financed dams in Africa.</p>',
+        '<p><div class="flourish-embed flourish-chart" data-src="visualisation/10545564"><script src="https://public.flourish.studio/resources/embed.js"></script></div></p><p>Enter China. The Chinese have already provided finance for two gas power plants in Nigeria.</p><p>But one of its biggest investments in the energy sector is the new 700MW Zungera Hydroelectric dam located in Niger State, which is due to come on stream in 2022. It will meet close to 10% of Nigeria’s total domestic energy needs, according to Nigeria’s <a href="https://www.bpe.gov.ng/zungeru-hydropower-project/" target="_blank">Bureau of Public Enterprise</a></p><p>Zungera is one of 47 hydroelectric power projects China has been involved with in Africa since 2000, according to AidData.</p><p>These projects range in size from the massive 2,172MW Caculo Cabaca Hydroelectric power station in Angola to the 0.64MW Makali plant in Sierra Leone. Together they have a potential generating capacity of close to 12,000MW.</p><p>Zungera is the sixth largest of Chinese-financed dams in Africa.</p>',
       location: {
         center: [6.2932451, 9.9005702],
         zoom: 12,
@@ -302,6 +298,31 @@ var config = {
       image: './images/Opéra_d_Alger.png',
       description:
         '<p>China has been involved in the construction or rehabilitation of three other theatres in Africa, one in Cote d’Ivoire, one in Ghana and one in Senegal.</p><p>But the leisure activity it has built the most infrastructure for is sport. Football is the most popular sport in Africa. China has financed the construction or rehabilitation of 62 stadiums around the continent over the past two decades, particularly in West Africa.</p>',
+      location: {
+        center: [2.9303258, 36.7368171],
+        zoom: 14,
+        pitch: 60,
+        bearing: 0,
+        // flyTo additional controls-
+        // These options control the flight curve, making it move
+        // slowly and zoom out almost completely before starting
+        // to pan.
+        //speed: 2, // make the flying slow
+        //curve: 1, // change the speed at which it zooms out
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+
+    {
+      id: 'algeria3',
+      alignment: 'right',
+      hidden: false,
+      description:
+        '<p><div class="flourish-embed flourish-map" data-src="visualisation/10545629"><script src="https://public.flourish.studio/resources/embed.js"></script></div></p><p>China has been involved in the construction or rehabilitation of three other theatres in Africa, one in Cote d’Ivoire, one in Ghana and one in Senegal.</p><p>But the leisure activity it has built the most infrastructure for is sport. Football is the most popular sport in Africa. China has financed the construction or rehabilitation of 62 stadiums around the continent over the past two decades, particularly in West Africa.</p>',
       location: {
         center: [2.9303258, 36.7368171],
         zoom: 14,
